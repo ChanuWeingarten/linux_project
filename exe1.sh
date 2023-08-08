8bb7c38e1910021154c727f38488383532ec0c4f
ChanuWeingarten <c0583260972@gmail.com>
2023-08-08 13:32:16 +0300

    exe1 work

--- exe1.sh
+++ exe1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Validate input arguments
-if [ $# -eq 2 ]; then
+if [ $# -lt 2 ]; then
   echo "Usage: down_zip.sh <Target Directory> <Image URL1> [<Image URL2> ...]"
   exit 1
 fi
@@ -30,15 +30,9 @@ done
 archive_name=$(date +'%Y%m%d%H%M%S').zip
 archive_path="$1/$archive_name"
 
-# Check if 'zip' command is available
-if ! command -v zip &>/dev/null; then
-  echo "Error: 'zip' command not found."
-  echo "Please install 'zip' and try again."
-  exit 1
-fi
 
 # Create the archive
-if zip -r "$archive_path" "$temp_dir" &>/dev/null; then
+if zip -r "$archive_path" "$temp_dir"; then
   echo "Archive created: $archive_name"
 else
   echo "Failed to create archive: $archive_name"
@@ -49,3 +43,4 @@ fi
 rm -rf "$temp_dir"
 
 echo "Process completed successfully."
+
